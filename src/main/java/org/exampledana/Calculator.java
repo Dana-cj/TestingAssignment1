c97e04913c3ba831a2ce4b14bbc015ddc74cce90
Daniela <danielastefania.dumitru2015@gmail.com>
2024-06-03 13:09:28 +0300

    throw exception if incorrect format

--- src/main/java/org/exampledana/Calculator.java
+++ src/main/java/org/exampledana/Calculator.java
@@ -5,23 +5,37 @@ import java.util.List;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
 
 public class Calculator {
 
     public static void main(String[] args) {
         Scanner scan= new Scanner(System.in);
-        System.out.println("""
-        This calculator is capable of computing a metric distance value from an expression that contains different scales and systems (supported units: mm, cm, dm, m, km)
-        EXAMPLE: 10 cm + 1 m - 10 mm = 1090 mm
-        Please type the expression that you want to be computed:
-        """);
-        String expression = scan.nextLine();
-        System.out.println("You can also choose the unit of the output from the following supported formats: mm, cm, dm, m, km. Type your option:");
-        Unit unit = getUnit(scan);
-        calculateDistance(expression, unit);
-        //calculateDistance("20m-10cm+2m-4m", Unit.mm);
-    }
+        boolean formatObserved= false;
+        while (!formatObserved) {
+            try {
+                System.out.println("""
+                        This calculator is capable of computing a metric distance value from an expression that contains different scales and systems (supported units: mm, cm, dm, m, km)
+                        Note: You can use upper case letters, lower case letters and white spaces 
+                        EXAMPLE: 10 cm + 1 m - 10 mm = 1090 mm 
+                        Please type the expression that you want to be computed:
+                        """);
+                String expression = scan.nextLine();
+                System.out.println("You can also choose the unit of the output from the following supported formats: mm, cm, dm, m, km (upper case or lower case letters). Type your option:");
+                Unit unit = getUnit(scan);
+                calculateDistance(expression, unit);
+                formatObserved = true;
 
+            } catch (PatternSyntaxException exception) {
+                System.out.println("""
+                        Wrong format! The expression should have at least a group that contains:
+                                                                            - a symbol '+' or '-'
+                                                                            - a value 
+                                                                            - an unit
+                        """);
+            }
+        }
+    }
     static Unit getUnit(Scanner scan) {
         Unit unit=null;
         do {
@@ -34,12 +48,12 @@ public class Calculator {
         return unit;
     }
 
-    static double calculateDistance(String expression, Unit unit){
+    static double calculateDistance(String expression, Unit unit) throws PatternSyntaxException{
         if (!expression.startsWith("-")){
             expression="+"+expression;
         }
         Pattern pattern = Pattern.compile("(\\+|\\-)(\\d+)(mm|cm|dm|m|km)");
-        Matcher matcher=pattern.matcher(expression);
+        Matcher matcher=pattern.matcher(expression.toLowerCase().replace(" ", ""));
         List<String> listOfComputations= new ArrayList<>();
         while (matcher.find()){
             for (int i=1; i<=3; i++) {
@@ -47,7 +61,9 @@ public class Calculator {
                 //System.out.println(matcher.group(i));
             }
         }
-        if(listOfComputations.size()==0)System.out.println("Wrong format of the expression.Try again!");
+        if(listOfComputations.size()==0) {
+            throw new PatternSyntaxException("Wrong format of the expression.Try again!","(\\+|\\-)(\\d+)(mm|cm|dm|m|km)", 0);
+        }
         double value=0;
         for (int i=0; i<=listOfComputations.size()-3; i++){
             if(listOfComputations.get(i).equals("+")){
@@ -60,7 +76,7 @@ public class Calculator {
             }
         }
         value/= unit.getValue();
-        // System.out.println(value);
+        System.out.println(value);
         return value;
     }
 }
--- src/test/java/org/exampledana/CalculatorTest.java
+++ src/test/java/org/exampledana/CalculatorTest.java
@@ -3,6 +3,8 @@ package org.exampledana;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import java.util.Scanner;
 
@@ -17,24 +19,44 @@ public class CalculatorTest {
         String input3="CM";
         Assertions.assertEquals(Unit.cm, Calculator.getUnit(new Scanner(input3)));
     }
-
+    @Test
+    public void conversionWorks() {
+        Assertions.assertEquals((-1000), Calculator.calculateDistance("-1m", Unit.mm));
+    }
+    @Test
+    public void conversionWithUpperCaseAndLowerCaseLettersWorks() {
+        Assertions.assertEquals((2), Calculator.calculateDistance("20 cM", Unit.dm));
+    }
     @Test
     public void additionWorks() {
         Assertions.assertEquals((10200), Calculator.calculateDistance("10m+20cm", Unit.mm));
     }
     @Test
-    public void additionAndSubtractionWorks() {
-        Assertions.assertEquals((9), Calculator.calculateDistance("-1m+10m-200cm-1m+3m", Unit.m));
+    public void additionWitWhiteSpacesWorks() {
+        Assertions.assertEquals((10200), Calculator.calculateDistance("+ 10 m + 20 cm", Unit.mm));
     }
     @Test
     public void subtractionWorks() {
+       // Assertions.assertEquals((0.8), Calculator.calculateDistance("1m-2dm", Unit.m));
         Assertions.assertEquals((18), Calculator.calculateDistance("-20mm+20cm", Unit.cm));
     }
-//    @ParameterizedTest
-//    @CsvSource(value = {"test:test", "tEst:test", "Java:java"}, delimiter = ':')
-//    @CsvSource(value = {""10m+20cm": 10200", "10m+20m": "30000"}, delimiter = ':')
-//    public void parameterizedAdditionWorks(String expression, Unit unit, double result){
-//        Assertions.assertEquals(result, Calculator.calculateDistance(expression, Unit.mm));
-//    }
+    @Test
+    public void additionAndSubtractionWorks() {
+        Assertions.assertEquals((9), Calculator.calculateDistance("-1m+10m-200cm-1m+3m", Unit.m));
+        Assertions.assertEquals((0.01), Calculator.calculateDistance("1km-990m", Unit.km));
+        Assertions.assertEquals((1), Calculator.calculateDistance("2dm-10cm", Unit.dm));
+    }
+    @Test
+    public void additionAndSubtractionWithWhiteSpacesWorks() {
+        Assertions.assertEquals((9), Calculator.calculateDistance("-1 m+10 m-200 cm-1 m+3 m", Unit.m));
+        Assertions.assertEquals((0.01), Calculator.calculateDistance(" 1 km - 990 m", Unit.km));
+        Assertions.assertEquals((1), Calculator.calculateDistance(" 2 dm -  10 cm ", Unit.dm));
+    }
+    @ParameterizedTest
+    @CsvSource({"10m+20cm-10cm, 10100", "2 dm -  10 cm, 100", "1KM-200m, 800000", "1cm, 10", "-2mm+4mm, 2"})
+    //@CsvSource(value = {"10m+20cm"= 10200}, delimiter = ':')
+    public void parameterizedAdditionWorks(String expression, double result){
+        Assertions.assertEquals(result, Calculator.calculateDistance(expression, Unit.mm));
+    }
 
 }
